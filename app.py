e8dde3a30e6d36aaa3b83a4664321a6644893865
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 16:04:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,44 @@
-
 import cv2
 import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+import numpy as np
+from moviepy.editor import VideoFileClip
 
+# Helper function to apply filters to video frames
+def apply_filter(frame, filter_type):
+    if filter_type == "Grayscale":
+        return cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    elif filter_type == "Canny Edge Detection":
+        return cv2.Canny(frame, 100, 200)
+    # Add more filter options here
 
-class VideoTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.threshold1 = 100
-        self.threshold2 = 200
+    # If no filter selected or unknown filter type, return original frame
+    return frame
 
-    def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+# Streamlit app
+def main():
+    st.title("Video Filter App")
+    
+    # Upload video file
+    video_file = st.file_uploader("Upload a video", type=["mp4", "avi"])
+    if video_file is not None:
+        clip = VideoFileClip(video_file)
+        duration = clip.duration
 
-        img = cv2.cvtColor(
-            cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR
-        )
+        # Limit video duration to 10 seconds
+        if duration > 10.0:
+            st.warning("Video duration exceeds the maximum allowed duration (10 seconds). Please upload a shorter video.")
+        else:
+            # Display video player
+            st.video(video_file)
 
-        return img
+            # Select filter option
+            filter_type = st.selectbox("Select filter", ["None", "Grayscale", "Canny Edge Detection"])
 
+            # Process video frames and display the filtered result
+            for frame in clip.iter_frames():
+                filtered_frame = apply_filter(frame, filter_type)
+                st.image(filtered_frame)
 
-ctx = webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
-
-if ctx.video_transformer:
-    ctx.video_transformer.threshold1 = st.slider("Threshold1", 0, 1000, 100)
-    ctx.video_transformer.threshold2 = st.slider("Threshold2", 0, 1000, 200)
+# Run the Streamlit app
+if __name__ == "__main__":
+    main()
