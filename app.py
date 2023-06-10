b4a58a98bcb9dd43852342f2ec451cb7a9f295d5
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 15:50:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,61 +1,14 @@
-import streamlit as st
 import cv2
-from moviepy.editor import *
-import numpy as np
-import base64
+from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
 
-def extract_frames(video_file):
-    video_capture = cv2.VideoCapture(video_file)
 
-    # Rest of the frame extraction code...
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        img = frame.to_ndarray(format="bgr24")
 
-def add_text_overlay(video_file):
-    # Text overlay code...
+        img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
 
-def apply_blur_effect(video_file):
-    # Blur effect code...
+        return img
 
-def download_video(video_path):
-    with open(video_path, "rb") as video_file:
-        video_bytes = video_file.read()
-        video_base64 = base64.b64encode(video_bytes).decode("utf-8")
-        href = f'<a href="data:video/mp4;base64,{video_base64}" download="output.mp4">Download edited video</a>'
-        st.markdown(href, unsafe_allow_html=True)
 
-def main():
-    st.title("Video Editor")
-
-    # Upload video
-    uploaded_file = st.file_uploader("Upload a video", type=['mp4', 'mov'])
-
-    if uploaded_file is not None:
-        # Get the file name
-        file_name = uploaded_file.name
-
-        # Save the uploaded file
-        with open(file_name, "wb") as file:
-            file.write(uploaded_file.getbuffer())
-
-        # Video editing options
-        options = ["Extract Frames", "Add Text Overlay", "Apply Blur Effect"]
-        selected_option = st.selectbox("Select an option", options)
-
-        if selected_option == "Extract Frames":
-            extract_frames(file_name)
-            st.success("Frames extracted successfully.")
-
-        elif selected_option == "Add Text Overlay":
-            add_text_overlay(file_name)
-            st.success("Text overlay applied successfully.")
-
-        elif selected_option == "Apply Blur Effect":
-            apply_blur_effect(file_name)
-            st.success("Blur effect applied successfully.")
-
-        st.video(file_name)
-
-        if st.button("Download"):
-            download_video(file_name)
-
-if __name__ == '__main__':
-    main()
+webrtc_streamer(key="example", video_transformer_factory=VideoTransformer)
