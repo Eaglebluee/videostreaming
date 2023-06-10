774c972baecb0628f899b619e37c60bff819e973
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 17:47:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 import streamlit as st
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, VideoTransformerBase, RTCConfiguration
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+class VideoTransformer(VideoTransformerBase):
+    def transform(self, frame):
+        # Apply transformations to the frame (optional)
+        return frame
 
 st.title("My first Streamlit app")
 st.write("Hello, world")
 
-webrtc_streamer(key="example", video=True, audio=False, 
-                client_settings={"constraints": {"video": True, "audio": False}})
-
+webrtc_streamer(
+    key="example",
+    mode="video",
+    rtc_configuration=RTC_CONFIGURATION,
+    video_transformer_factory=VideoTransformer,
+)
