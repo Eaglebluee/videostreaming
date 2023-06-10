600fc6a9bd13240aa4718965b2d636483c739c18
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 14:42:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,31 +3,26 @@ import numpy as np
 import streamlit as st
 from streamlit_webrtc import VideoTransformerBase, webrtc_streamer, WebRtcMode
 
+
 class PencilSketchTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.canvas = None
-
     def transform(self, frame):
-        img = frame.to_ndarray(format="bgr24")
+        # Convert the BGR frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # Invert the grayscale image
+        invert = cv2.bitwise_not(gray)
+        # Apply Gaussian blur to the inverted image
+        blur = cv2.GaussianBlur(invert, (111, 111), 0)
+        # Blend the grayscale image and blurred image using color dodge blend mode
+        pencil_sketch = cv2.divide(gray, 255 - blur, scale=256.0)
+        # Convert the pencil sketch back to BGR
+        sketch_bgr = cv2.cvtColor(pencil_sketch, cv2.COLOR_GRAY2BGR)
+        return sketch_bgr
 
-        # Apply pencil sketch effect
-        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        blurred_img = cv2.GaussianBlur(gray_img, (21, 21), 0, 0)
-        inverted_img = cv2.bitwise_not(blurred_img)
-        img_sketch = cv2.divide(gray_img, inverted_img, scale=256.0)
-
-        if self.canvas is None:
-            self.canvas = np.zeros_like(img_sketch)
-
-        img_blend = cv2.multiply(img_sketch, self.canvas, scale=1 / 256.0)
-        img_blend = cv2.cvtColor(img_blend, cv2.COLOR_GRAY2BGR)
-
-        self.canvas = cv2.add(img_blend, img, dtype=cv2.CV_8UC3)
-        return self.canvas
 
 def main():
-    st.title("Real-Time Pencil Sketch")
+    st.title("Real-time Pencil Sketch Effect")
 
+    # Create a WebRTC pipeline
     webrtc_ctx = webrtc_streamer(
         key="example",
         mode=WebRtcMode.SENDRECV,
@@ -35,9 +30,14 @@ def main():
         async_transform=True,
     )
 
+    # Display the transformed video frames
     if webrtc_ctx.video_transformer:
-        st.write("Webcam Video")
-        st.video(webrtc_ctx.video_transformer)
+        st.image(webrtc_ctx.video_transformer)
+
+    st.markdown("---")
+    st.markdown("### Instructions")
+    st.markdown("1. Click the **Start** button to start the video stream.")
+    st.markdown("2. Apply the **Pencil Sketch** effect using the menu on the left.")
 
 if __name__ == "__main__":
     main()
