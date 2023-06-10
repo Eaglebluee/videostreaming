4b19c37c80ef91e9a34bc0d9e149c0cfa4415f4c
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 20:35:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,165 +1,57 @@
+import streamlit as st
 import cv2
 import numpy as np
-from PIL import Image
-import io
-import streamlit as st
-from streamlit_webrtc import VideoTransformerBase, webrtc_streamer
+from moviepy.editor import VideoFileClip
 
+def apply_sepia_filter(frame):
+    # Apply sepia filter to the frame
+    sepia_matrix = np.array([[0.272, 0.534, 0.131],
+                             [0.349, 0.686, 0.168],
+                             [0.393, 0.769, 0.189]])
+    filtered_frame = cv2.transform(frame, sepia_matrix)
 
-class VideoTransformer(VideoTransformerBase):
-    def __init__(self):
-        self.selected_filter = "None"
-        self.blur_rate = 0.5
-        self.brightness_amount = 0
-        self.apply_enhancement_filter = False
-
-    def brighten_image(self, frame, amount):
-        img_bright = cv2.convertScaleAbs(frame, beta=amount)
-        return img_bright
-
-    def blur_image(self, frame, amount):
-        blur_img = cv2.GaussianBlur(frame, (11, 11), amount)
-        return blur_img
-
-    def enhance_details(self, frame):
-        hdr = cv2.detailEnhance(frame, sigma_s=12, sigma_r=0.15)
-        return hdr
-
-    def cartoon_effect(self, frame):
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray_blur = cv2.medianBlur(gray, 7)
-        edges = cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 7, 7)
-        color = cv2.bilateralFilter(frame, 9, 250, 250)
-        cartoon_img = cv2.bitwise_and(color, color, mask=edges)
-        return cartoon_img
-
-    def greyscale(self, frame):
-        greyscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        return greyscale
-
-    def sepia(self, frame):
-        frame_sepia = np.array(frame, dtype=np.float64)
-        frame_sepia = cv2.transform(frame_sepia, np.matrix([[0.272, 0.534, 0.131],
-                                                            [0.349, 0.686, 0.168],
-                                                            [0.393, 0.769, 0.189]]))
-        frame_sepia[np.where(frame_sepia > 255)] = 255
-        frame_sepia = np.array(frame_sepia, dtype=np.uint8)
-        return frame_sepia
-
-    def pencil_sketch_grey(self, frame):
-        sk_gray, sk_color = cv2.pencilSketch(frame, sigma_s=60, sigma_r=0.07, shade_factor=0.1)
-        return sk_gray
-
-    def invert(self, frame):
-        inv = cv2.bitwise_not(frame)
-        return inv
-
-    def summer(self, frame):
-        summer_frame = frame.copy()
-        summer_frame[..., 0] = np.clip(frame[..., 0] * 1.2, 0, 255)
-        summer_frame[..., 2] = np.clip(frame[..., 2] * 0.8, 0, 255)
-        return summer_frame
-
-    def winter(self, frame):
-        winter_frame = frame.copy()
-        winter_frame[..., 0] = np.clip(frame[..., 0] * 0.8, 0, 255)
-        winter_frame[..., 2] = np.clip(frame[..., 2] * 1.2, 0, 255)
-        return winter_frame
-
-    def transform(self, frame):
-        if self.selected_filter == "Cartoon Effect":
-            processed_frame = self.cartoon_effect(frame)
-        elif self.selected_filter == "Gray Effect":
-            processed_frame = self.greyscale(frame)
-        elif self.selected_filter == "Sepia Effect":
-            processed_frame = self.sepia(frame)
-        elif self.selected_filter == "Pencil Sketch":
-            processed_frame = self.pencil_sketch_grey(frame)
-        elif self.selected_filter == "Invert Effect":
-            processed_frame = self.invert(frame)
-        elif self.selected_filter == "Summer":
-            processed_frame = self.summer(frame)
-        elif self.selected_filter == "Winter":
-            processed_frame = self.winter(frame)
-        else:
-            processed_frame = np.copy(frame)
-
-        processed_frame = self.blur_image(processed_frame, self.blur_rate)
-        processed_frame = self.brighten_image(processed_frame, self.brightness_amount)
-
-        if self.apply_enhancement_filter:
-            processed_frame = self.enhance_details(processed_frame)
-
-        return processed_frame
-
-    def transform_video(self, video):
-        frames = []
-        for frame in video:
-            processed_frame = self.transform(frame)
-            frames.append(processed_frame)
-        return frames
-
+    return filtered_frame
 
 def main():
     st.title("Video Editor")
-    st.subheader("You can edit and apply filters to your videos!")
 
-    filters = {
-        "None": "No filter applied",
-        "Cartoon Effect": "Apply a cartoon effect to the video",
-        "Gray Effect": "Convert the video to grayscale",
-        "Sepia Effect": "Apply a sepia tone effect to the video",
-        "Pencil Sketch": "Create a pencil sketch effect",
-        "Invert Effect": "Invert the colors of the video",
-        "Summer": "Apply a summer color effect",
-        "Winter": "Apply a winter color effect"
-    }
+    # Upload video
+    video_file = st.file_uploader("Upload a video", type=["mp4"])
 
-    selected_filter = st.sidebar.selectbox("Filters", list(filters.keys()))
-    filter_tooltip = filters[selected_filter]
-    st.sidebar.text(filter_tooltip)
+    if video_file is not None:
+        st.subheader("Original Video")
+        st.video(video_file)
 
-    blur_rate = st.sidebar.slider("Blurring", min_value=0.5, max_value=3.5)
-    brightness_amount = st.sidebar.slider("Brightness", min_value=-50, max_value=50, value=0)
-    apply_enhancement_filter = st.sidebar.checkbox('Enhance Details')
+        # Process video
+        video_bytes = video_file.read()
+        cap = cv2.VideoCapture(video_bytes)
+        frames = []
 
-    uploaded_file = st.file_uploader("Upload a video file", type=["mp4", "mov"])
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-    if uploaded_file is not None:
-        video_bytes = uploaded_file.read()
-        video_np = np.frombuffer(video_bytes, np.uint8)
-        video_cv = cv2.imdecode(video_np, cv2.IMREAD_UNCHANGED)
-        video_transformer = VideoTransformer()
-        video_transformer.selected_filter = selected_filter
-        video_transformer.blur_rate = blur_rate
-        video_transformer.brightness_amount = brightness_amount
-        video_transformer.apply_enhancement_filter = apply_enhancement_filter
+            # Apply sepia filter
+            filtered_frame = apply_sepia_filter(frame)
 
-        if video_cv is not None:
-            processed_frames = video_transformer.transform_video(video_cv)
+            # Add filtered frame to the list
+            frames.append(filtered_frame)
 
-            # Display the original video
-            st.subheader("Original Video")
-            st.video(uploaded_file)
+        # Save edited video
+        edited_video_path = "edited_video.mp4"
+        out = cv2.VideoWriter(edited_video_path, cv2.VideoWriter_fourcc(*"mp4v"), cap.get(cv2.CAP_PROP_FPS),
+                              (frames[0].shape[1], frames[0].shape[0]))
 
-            # Display the edited video
-            st.subheader("Edited Video")
-            edited_video_bytes = encode_video(processed_frames)
-            st.video(edited_video_bytes)
+        for frame in frames:
+            out.write(frame)
 
-def encode_video(frames):
-    # Convert frames to bytes for streaming
-    edited_video = cv2.VideoWriter_fourcc(*"mp4v")
-    frame_shape = frames[0].shape[:2][::-1]
-    edited_video_stream = cv2.VideoWriter("edited_video.mp4", edited_video, 30, frame_shape)
-    for frame in frames:
-        edited_video_stream.write(frame)
-    edited_video_stream.release()
+        out.release()
+        cap.release()
 
-    with open("edited_video.mp4", "rb") as file:
-        video_bytes = file.read()
+        # Display edited video
+        st.subheader("Edited Video")
+        st.video(edited_video_path)
 
-    return video_bytes
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
