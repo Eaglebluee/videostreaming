21ac259fe371a9c1912394bc63130af5b2650110
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 15:12:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,43 +2,23 @@ import streamlit as st
 import cv2
 import numpy as np
 
-def pencil_sketch(video_frame):
-    gray_frame = cv2.cvtColor(video_frame, cv2.COLOR_BGR2GRAY)
-    inverted_frame = cv2.bitwise_not(gray_frame)
-    blurred_frame = cv2.GaussianBlur(inverted_frame, (111, 111), 0)
-    inverted_blurred_frame = cv2.bitwise_not(blurred_frame)
-    sketch_frame = cv2.divide(gray_frame, inverted_blurred_frame, scale=256.0)
-    return cv2.cvtColor(sketch_frame, cv2.COLOR_GRAY2BGR)
+def pencil_sketch(video_file):
+    video_capture = cv2.VideoCapture(video_file.name)
 
-def main():
-    st.title("Video Filters")
+    while video_capture.isOpened():
+        ret, frame = video_capture.read()
+        if not ret:
+            break
+        
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        sketch_frame = cv2.cvtColor(gray_frame, cv2.COLOR_GRAY2BGR)
+
+        st.image(sketch_frame, channels="BGR")
+
+if __name__ == "__main__":
+    st.title("Video Upload")
 
     uploaded_file = st.file_uploader("Upload a video", type=['mp4', 'mov'])
 
     if uploaded_file is not None:
-        try:
-            # Read the video file
-            video_bytes = uploaded_file.read()
-            video_nparray = np.frombuffer(video_bytes, np.uint8)
-            video_capture = cv2.imdecode(video_nparray, cv2.IMREAD_UNCHANGED)
-
-            if video_capture is not None:
-                # Display the original video
-                st.video(video_capture)
-
-                # Dropdown menu for filters
-                filter_option = st.selectbox("Filters", ["None", "Pencil Sketch"])
-
-                # Apply selected filter to the video
-                if filter_option == "Pencil Sketch":
-                    sketch_video = pencil_sketch(video_capture)
-                    st.video(sketch_video)
-                else:
-                    st.warning("Please select a valid filter option.")
-            else:
-                st.error("Failed to read the video file.")
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-
-if __name__ == '__main__':
-    main()
+        pencil_sketch(uploaded_file)
