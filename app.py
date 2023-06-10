5df62472cc57433c243d836977a44d1f6ddaf925
Eaglebluee <134376608+Eaglebluee@users.noreply.github.com>
2023-06-10 16:17:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import tempfile
 import moviepy.editor as mp
+import cv2
+import numpy as np
 
 # Set maximum video duration in seconds
 MAX_DURATION = 15
@@ -21,10 +23,39 @@ if uploaded_file is not None:
     if video_duration <= MAX_DURATION:
         # Process the video
         st.success("Video uploaded successfully!")
-        st.video(temp_file.name)
+
+        # Apply canine filter to the video frames
+        video_clip = mp.VideoFileClip(temp_file.name)
+        edited_clip = video_clip.fl_image(lambda frame: apply_canine_filter(frame))
+
+        # Save the edited video to a temporary file
+        edited_temp_file = tempfile.NamedTemporaryFile(delete=False)
+        edited_clip.write_videofile(edited_temp_file.name, codec="libx264")
+
+        # Display the original and edited videos
+        col1, col2 = st.beta_columns(2)
+        with col1:
+            st.subheader("Original Video")
+            st.video(temp_file.name)
+
+        with col2:
+            st.subheader("Edited Video")
+            st.video(edited_temp_file.name)
+
+        # Remove the temporary files
+        temp_file.close()
+        edited_temp_file.close()
     else:
         # Video duration exceeds the allowed limit
         st.error(f"Video duration should be less than or equal to {MAX_DURATION} seconds.")
 
-    # Remove the temporary file
-    temp_file.close()
+def apply_canine_filter(frame):
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # Apply the canine filter
+    canine_filter = np.zeros_like(frame)
+    canine_filter[..., 2] = gray  # Set the red channel to the grayscale image
+    canine_filter[..., 1] = gray  # Set the green channel to the grayscale image
+
+    return canine_filter
